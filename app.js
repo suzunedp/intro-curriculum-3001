3ab240da4c0dfc843697b11dfad66c87dbd5cccf
Suzuki Takaharu <Takaharu@Suzuki-no-iMac.local>
2017-08-02 22:52:37 +0900

    解答

--- app.js
+++ app.js
@@ -7,9 +7,12 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    for (var i = 1; i <= n; i++) {
+      result = result*i
+    }
     return result;
 }
+
 const assert = require('assert');
 assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
 assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
